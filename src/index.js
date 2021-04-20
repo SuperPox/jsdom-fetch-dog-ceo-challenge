3979730eb5a497420f3ba7d02604e82955fc29a0
SuperPox <calebkohl@gmail.com>
2021-04-19 20:55:05 -0600

    commit tree fdf7882cb8ece52351bb6d6bc513797d306b1b66

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', function() 
+{
+    loadPics();
+    loadBreeds();
+});
+
+// CHALLENGE 1 ///////////////
+
+function loadPics()
+{
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch (imgUrl)
+    .then(res=> res.json())
+    .then(results => {results.message.forEach(pic => addPic(pic))});
+}
+
+function addPic(picUrl)
+{
+    let container = document.querySelector('#dog-image-container');
+    let newPic = document.createElement('img');
+    newPic.src = picUrl
+    container.appendChild(newPic)
+}
+
+// CHALLENGE 2 ///////////////
+
